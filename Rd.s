6f655ce57bd492ea908c6caa90e0e55790045bb7
DULCE JASMIN VILLALOBOS PEREZ <42878496+Villalobos39@users.noreply.github.com>
2020-02-27 14:08:59 -0800

    Create Rd.s

---
+++ Rd.s
@@ -0,0 +1,13 @@
+.datavar1:    .asciz   " %d\012"
+.text
+.global  main
+main:    
+push     {r4, lr}
+mov      r4, #0.L2:     
+mov      r1, r4
+ldr      r0, =var1
+add      r4, r4, #1
+bl        print
+fcmp      r4, #5
+bne      .L2
+pop      {r4, pc}
